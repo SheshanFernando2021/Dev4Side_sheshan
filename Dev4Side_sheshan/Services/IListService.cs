aa1a0d3bbca2d1266b94a39fd12c34bab04f6517
Sheshan Fernando <sithumindasheshan@gmail.com>
2025-07-05 15:21:57 +0200

    List Service completed

--- Dev4Side_sheshan/Services/IListService.cs
+++ Dev4Side_sheshan/Services/IListService.cs
@@ -1,6 +1,11 @@
-﻿namespace Dev4Side_sheshan.Services
+﻿using Dev4Side_sheshan.Models;
+
+namespace Dev4Side_sheshan.Services
 {
     public interface IListService
     {
+        Task<List<ListEntity>> GetListsAsync(int userId);
+        Task<ListEntity> CreateList(ListEntity listEntity);
+        Task DeleteList (int userId, int listId);
     }
 }
--- Dev4Side_sheshan/Services/ListService.cs
+++ Dev4Side_sheshan/Services/ListService.cs
@@ -1,6 +1,39 @@
-﻿namespace Dev4Side_sheshan.Services
+﻿using Dev4Side_sheshan.Models;
+using Dev4Side_sheshan.Repos;
+
+namespace Dev4Side_sheshan.Services
 {
-    public class ListService
+    public class ListService : IListService
     {
+        private readonly IListRepo _listRepo;
+
+        public ListService(IListRepo listRepo)
+        {
+            _listRepo = listRepo;
+        }
+
+        public async Task<ListEntity> CreateList(ListEntity listEntity)
+        {
+            return await _listRepo.createList(listEntity);
+        }
+
+        public async Task DeleteList(int userId, int listId)
+        {
+            var list = await _listRepo.getOneList(listId, userId);
+            if (list == null) {
+                throw new Exception("no list found to delete");
+            }
+            await _listRepo.deleteList(listId, userId);
+        }
+
+        public async Task<List<ListEntity>> GetListsAsync(int userId)
+        {
+            var list = await _listRepo.getAllListAsync(userId);
+            if (list == null)
+            {
+                throw new NotImplementedException("no lists found for this user");
+            }
+            return list;
+        }
     }
 }
