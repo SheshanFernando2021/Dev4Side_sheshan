13c8ce045d97e3669a3cd82f7e99ed36dc98fab1
Sheshan Fernando <sithumindasheshan@gmail.com>
2025-07-05 19:18:05 +0200

    added DTOs

---
+++ Dev4Side_sheshan/DTOs/UserRegisterDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dev4Side_sheshan.DTOs
+{
+    public class UserRegisterDTO
+    {
+        [Required]
+        public string Email { get; set; } = null!;
+        [Required]
+        public string Password { get; set; } = null!;
+        [Required]
+        [Compare("Password")]
+        public string PasswordConfirm { get; set; } = null!;
+        public string username => Email.Contains('@') ? Email.Split('@')[0] : Email;
+    }
+}
