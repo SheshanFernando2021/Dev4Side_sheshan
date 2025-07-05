603439fdfcbbd23a76b309babe055760b354b202
Sheshan Fernando <sithumindasheshan@gmail.com>
2025-07-05 12:52:54 +0200

    List repo completed

--- Dev4Side_sheshan/Repos/IListRepo.cs
+++ Dev4Side_sheshan/Repos/IListRepo.cs
@@ -5,9 +5,9 @@ namespace Dev4Side_sheshan.Repos
     public interface IListRepo
     {
         Task<List<ListEntity>> getAllListAsync(int userId);
-        Task<ListEntity> getOneList(int listId);
+        Task<ListEntity> getOneList(int listId, int userId);
         Task<ListEntity> createList(ListEntity listEntity);
         Task<ListEntity> updateList(ListEntity listEntity);
-        Task deleteList (int listId);
+        Task deleteList (int listId, int userId);
     }
 }
--- Dev4Side_sheshan/Repos/ListRepo.cs
+++ Dev4Side_sheshan/Repos/ListRepo.cs
@@ -1,6 +1,53 @@
-﻿namespace Dev4Side_sheshan.Repos
+﻿using Dev4Side_sheshan.Data;
+using Dev4Side_sheshan.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dev4Side_sheshan.Repos
 {
-    public class ListRepo
+    public class ListRepo : IListRepo
     {
+        private readonly AppDbContext _db;
+
+        public ListRepo(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<ListEntity> createList(ListEntity listEntity)
+        {
+            _db.Lists.Add(listEntity);
+            await _db.SaveChangesAsync();
+            return listEntity;
+        }
+
+        public async Task deleteList(int listId, int userId)
+        {
+            var list = await getOneList(listId, userId);
+            if (list != null)
+            {
+                _db.Lists.Remove(list);
+                await _db.SaveChangesAsync();
+            }
+        }
+
+        public async Task<List<ListEntity>> getAllListAsync(int userId)
+        {
+            return await _db.Lists
+                            .Where(l => l.UserId == userId)
+                            .ToListAsync();
+        }
+
+        public async Task<ListEntity> getOneList(int listId , int userId)
+        {
+            return await _db.Lists
+                            .FirstOrDefaultAsync(l => l.UserId == userId && l.ListId == listId);
+        }
+
+        public async Task<ListEntity> updateList(ListEntity listEntity)
+        {
+           _db.Lists.Update(listEntity);
+            await _db.SaveChangesAsync();
+            return listEntity;
+        }
     }
 }
