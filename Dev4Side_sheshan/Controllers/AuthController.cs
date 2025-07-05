b00d064f14ba35cd3deac0846b7ffba616019871
Sheshan Fernando <sithumindasheshan@gmail.com>
2025-07-05 21:40:17 +0200

    Auth controller

---
+++ Dev4Side_sheshan/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Dev4Side_sheshan.DTOs;
+using Dev4Side_sheshan.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dev4Side_sheshan.Controllers
+{
+    [Route("auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] UserRegisterDTO userRegisterDTO)
+        {
+            await _authService.RegisterAsync(userRegisterDTO);
+            return Ok(new {message = $"User {userRegisterDTO.Email} : Registration successful 🎉"});
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserLoginDTO userLoginDTO)
+        {
+            var token = await _authService.LoginAsync(userLoginDTO);
+            return Ok(new AuthResponse { Token = token });
+        }
+    }
+}
