c16c8de63deff29053677382cb687643f8d24542
Sheshan Fernando <sithumindasheshan@gmail.com>
2025-07-05 13:09:00 +0200

    User repo completed

--- Dev4Side_sheshan/Repos/UserRepo.cs
+++ Dev4Side_sheshan/Repos/UserRepo.cs
@@ -1,6 +1,28 @@
-﻿namespace Dev4Side_sheshan.Repos
+﻿using Dev4Side_sheshan.Data;
+using Dev4Side_sheshan.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dev4Side_sheshan.Repos
 {
-    public class UserRepo
+    public class UserRepo: IUserRepo
     {
+        private readonly AppDbContext _db;
+
+        public UserRepo(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<UserEntity> createAsync(UserEntity user)
+        {
+            _db.Users.Add(user);
+            await _db.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<UserEntity> getUserName(string userName)
+        {
+            return await _db.Users.FirstOrDefaultAsync(u => u.Email == userName);
+        }
     }
 }
